8ceb6cebb9477c7146c8f9e9985f631bfd7e6664
Sushmitha436 <gangarapu.susmitha2002@gmail.com>
2021-12-07 11:33:33 +0000

    commited

---
+++ Currency.java
@@ -0,0 +1,10 @@
+public class Currency {
+    public static void main(String[] args) {
+        int n=1234;
+        System.out.println("India: \u20A9" +n);
+        System.out.println("China: \u00A5" +n);
+        System.out.println("France:\u20AC" +n);
+        System.out.println("USA: \u0024" +n);
+}
+
+    }
